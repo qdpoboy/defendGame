64e80031095e07c79e22c2483f872c06597220f2
qdpoboy <280597526@qq.com>
2020-07-08 08:54:52 +0800

    子弹

--- assets/script/game.js
+++ assets/script/game.js
@@ -9,17 +9,31 @@ cc.Class({
         launcherPrefab1: {
             default: null,
             type: cc.Prefab
+        },
+        bulletPrefab: {
+            default: null,
+            type: cc.Prefab
         }
     },
 
     onLoad() {
         this.initLauncher();
+        this.setBullet();
     },
 
     //初始化发射器
     initLauncher() {
         let launcherPrefab = cc.instantiate(this.launcherPrefab1);
         this.launcherNode.addChild(launcherPrefab);
+    },
+
+    //设置子弹相关属性
+    setBullet() {
+        let bulletPrefab = cc.instantiate(this.bulletPrefab);
+        this.launcherNode.addChild(bulletPrefab);
+        //为了让发射器挡住子弹
+        bulletPrefab.zIndex = -10;
+        //
     },
 
     // start() { },
