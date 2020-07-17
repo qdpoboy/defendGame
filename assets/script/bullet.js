2af63c5d6616822d2b41bc6adf2238c137a0969d
qdpoboy <280597526@qq.com>
2020-07-17 15:14:08 +0800

    物理碰撞

--- assets/script/bullet.js
+++ assets/script/bullet.js
@@ -6,6 +6,8 @@ cc.Class({
 
     onLoad() {
         this.rigidBody = this.node.getComponent(cc.RigidBody);
+        //子弹碰撞道具次数
+        this.collisionWallCnt = 0;
     },
 
     updateBulletAngle() {
@@ -19,7 +21,17 @@ cc.Class({
 
     //碰撞开始接触时
     onBeginContact(contact, selfCollider, otherCollider) {
-        console.log('碰撞了' + otherCollider.tag);
+        if (otherCollider.tag >= 0 && otherCollider.tag <= 6) {//碰撞道具
+            console.log('碰撞了' + otherCollider.tag + '，减血');
+        } else if (otherCollider.tag > 6) {//碰撞墙体
+            this.collisionWallCnt++;
+            console.log('碰撞了' + otherCollider.tag + '，累加碰撞次数');
+        }
+        //碰撞墙体两次后子弹消失
+        if (this.collisionWallCnt == 2) {
+            this.node.destroy();
+            console.log('子弹消失');
+        }
     },
 
     // start() { },
