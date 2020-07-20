4e5d7b670fa179647ef96d6f67c09990d52c5211
qdpoboy <280597526@qq.com>
2020-07-20 17:31:08 +0800

    血量条

--- assets/script/bullet.js
+++ assets/script/bullet.js
@@ -6,31 +6,32 @@ cc.Class({
 
     onLoad() {
         this.rigidBody = this.node.getComponent(cc.RigidBody);
-        //子弹碰撞道具次数
+        //子弹碰撞墙体次数
         this.collisionWallCnt = 0;
     },
 
+    //设置子弹偏移角度
     updateBulletAngle() {
         this.node.angle = 45;
     },
 
+    //子弹射击
     shootBullet() {
         this.rigidBody.linearVelocity = cc.v2(200, 200);
-        console.log('子弹射击了');
     },
 
     //碰撞开始接触时
     onBeginContact(contact, selfCollider, otherCollider) {
         if (otherCollider.tag >= 0 && otherCollider.tag <= 6) {//碰撞道具
-            console.log('碰撞了' + otherCollider.tag + '，减血');
+            //碰撞道具减血
+            this.game.collisionBloodLoss(otherCollider.tag);
         } else if (otherCollider.tag > 6) {//碰撞墙体
             this.collisionWallCnt++;
-            console.log('碰撞了' + otherCollider.tag + '，累加碰撞次数');
         }
         //碰撞墙体两次后子弹消失
         if (this.collisionWallCnt == 2) {
             this.node.destroy();
-            console.log('子弹消失');
+            console.log('子弹销毁');
         }
     },
 
--- assets/script/game.js
+++ assets/script/game.js
@@ -13,12 +13,39 @@ cc.Class({
         bulletPrefab: {
             default: null,
             type: cc.Prefab
+        },
+        progressBar1: {
+            default: null,
+            type: cc.ProgressBar
+        },
+        progressBar2: {
+            default: null,
+            type: cc.ProgressBar
+        }
+        ,
+        progressBar3: {
+            default: null,
+            type: cc.ProgressBar
+        },
+        progressBar4: {
+            default: null,
+            type: cc.ProgressBar
+        },
+        progressBar5: {
+            default: null,
+            type: cc.ProgressBar
+        },
+        progressBar6: {
+            default: null,
+            type: cc.ProgressBar
         }
     },
 
     onLoad() {
+        this.initData();
         this.initPhysics();
         this.initLauncher();
+        this.initBaffle();
         this.newBullet();
     },
 
@@ -26,6 +53,30 @@ cc.Class({
         this.schedule(function () {
             this.newBullet();
         }, 2);
+    },
+
+    //初始化数据
+    initData() {
+        //每个按钮血量进度条
+        this.barArr = [];
+        this.barArr[1] = this.progressBar1;
+        this.barArr[2] = this.progressBar2;
+        this.barArr[3] = this.progressBar3;
+        this.barArr[4] = this.progressBar4;
+        this.barArr[5] = this.progressBar5;
+        this.barArr[6] = this.progressBar6;
+        //初始每个按钮血量
+        this.btnBloodArr = [];
+        this.btnBloodArr[1] = 50;
+        this.btnBloodArr[2] = 50;
+        this.btnBloodArr[3] = 50;
+        this.btnBloodArr[4] = 50;
+        this.btnBloodArr[5] = 50;
+        this.btnBloodArr[6] = 50;
+        //每个按钮剩余血量
+        this.btnNowBloodArr = [];
+        //每次子弹击中按钮的掉血量
+        this.stepBloodLoss = 1;
     },
 
     //初始化物理系统
@@ -42,6 +93,23 @@ cc.Class({
         this.launcherNode.addChild(launcherPrefab);
     },
 
+    //初始化卫星挡板
+    initBaffle() {
+        //this.schedule(this.baffleCircleMove, 0.01);
+    },
+
+    baffleCircleMove(dt) {
+        //https://blog.csdn.net/Jff316948714/article/details/81193722
+        // 先计算弧度
+        this.radian += dt * (this.carSpeed / 100);
+        let x = this.circleRadius * Math.cos(this.radian) + this.circleCenter.x;
+        let y = this.circleRadius * Math.sin(this.radian) + this.circleCenter.y;
+        let angle = 360 - 180 / Math.PI * this.radian;
+        this.sprCar.node.rotation = angle;
+        console.log('x = ' + x + '  y = ' + y + '  angle = ' + angle);
+        this.sprCar.node.position = cc.v2(x, y);
+    },
+
     //生成子弹相关属性
     newBullet() {
         let bulletPrefab = cc.instantiate(this.bulletPrefab);
@@ -51,7 +119,17 @@ cc.Class({
         this.bullutJs = bulletPrefab.getComponent('bullet');
         this.bullutJs.updateBulletAngle();
         this.bullutJs.shootBullet();
+        this.bullutJs.game = this;
     },
+
+    //碰撞减血
+    collisionBloodLoss(tag) {
+        if (this.barArr[tag]) {
+            let nowBlood = this.btnNowBloodArr[tag] ? this.btnNowBloodArr[tag] : this.btnBloodArr[tag];
+            this.btnNowBloodArr[tag] = nowBlood - this.stepBloodLoss;
+            this.barArr[tag].progress = this.btnNowBloodArr[tag] / this.btnBloodArr[tag];
+        }
+    }
 
     // update (dt) {},
 });
